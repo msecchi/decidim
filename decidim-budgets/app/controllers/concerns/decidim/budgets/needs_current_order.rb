07a1df6c011f36a5383188d9a429a9ee86cf6a82
Leonardo Diez <leiodd@gmail.com>
2020-04-27 16:27:56 +0200

    feat: refactor projects views using the new design

--- decidim-budgets/app/controllers/concerns/decidim/budgets/needs_current_order.rb
+++ decidim-budgets/app/controllers/concerns/decidim/budgets/needs_current_order.rb
@@ -9,7 +9,7 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
-        helper_method :current_order
+        helper_method :show_votes?, :finished?, :current_order, :can_have_order?, :checked_out?, :project_chosen?, :project_accepted?
 
         # The current order created by the user.
         #
@@ -24,6 +24,24 @@ module Decidim
 
         def persisted_current_order
           current_order if current_order&.persisted?
+        end
+
+        def can_have_order?
+          current_user.present? &&
+            current_settings.votes_enabled? &&
+            current_participatory_space.can_participate?(current_user) &&
+            allowed_to?(:create, :order, parent_component_context: parent_component_context)
+        end
+
+        delegate :show_votes?, to: :current_settings
+        delegate :checked_out?, to: :current_order, allow_nil: true
+
+        def finished?
+          !current_settings.votes_enabled? && show_votes?
+        end
+
+        def project_chosen?(project)
+          current_order&.projects&.include?(project)
         end
       end
     end

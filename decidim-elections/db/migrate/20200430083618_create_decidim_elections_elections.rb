0eaebd5bc5edc71b9d05a542c24390688e04568f
Leonardo Diez <leiodd@gmail.com>
2020-05-04 21:01:24 +0200

    feat: add the election model

---
+++ decidim-elections/db/migrate/20200430083618_create_decidim_elections_elections.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateDecidimElectionsElections < ActiveRecord::Migration[5.2]
+  def change
+    create_table :decidim_elections_elections do |t|
+      t.jsonb :title
+      t.jsonb :subtitle
+      t.jsonb :description
+      t.datetime :start_time
+      t.datetime :end_time
+      t.references :decidim_component, index: true
+    end
+  end
+end
